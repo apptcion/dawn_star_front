71060768a5ff247e9c65424de0763d6f3e1a6856
apptcion <apptcion@gmail.com>
2025-04-16 17:52:51 +0900

    update eco_brand tab ( merge with backend at EcoBrandProduct )

---
+++ lib/models/brand_model.dart
@@ -0,0 +1,33 @@
+import 'dart:convert';
+import 'dart:typed_data';
+
+import 'package:daystar/models/product_model.dart';
+
+class Brand {
+  final String id;
+  final String name;
+  final List<Product>? products;
+  late Uint8List brandImg;
+
+  Brand({
+    required this.id,
+    required this.name,
+    this.products,
+    required String brandImgStr
+  }){
+    this.brandImg = base64Decode(brandImgStr);
+  }
+
+  factory Brand.fromJSON(Map<String, dynamic> json) {
+    return Brand(
+      id: json['_id'],
+      name: json['brand_name'],
+      brandImgStr: json['logo'],
+      products: json['products'] != null
+          ? (json['products'] as List)
+          .map((product) => Product.fromJSON(product))
+          .toList()
+          : [],
+    );
+  }
+}
